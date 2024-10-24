e52009e996b3495e9ce2a277fa6d79ec8f1ba7ad
devops <*dhineshraj1998@gmail.com*>
2024-10-24 12:06:34 +0530

    React Connectivity Test Application

---
+++ Frontend/frontend-app/src/App.js
@@ -0,0 +1,16 @@
+
+import CustomerCrud from './component/CustomerCrud';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+function App() {
+  return (
+    <div>
+        <CustomerCrud/>
+    </div>
+  );
+}
+
+export default App;
